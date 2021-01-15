72d4137e064aad43dce406f8ef3274fe5bbc88a4
AdiatsaPutra <santikosetyo77@gmail.com>
2021-01-16 04:48:00 +0700

    Add project files.

---
+++ ServerChatApp_073_Adiatsa Putra Santika/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerChatApp_073_Adiatsa_Putra_Santika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostobjek = null;
+            try
+            {
+                hostobjek = new ServiceHost(typeof(ServiceCallback));
+                hostobjek.Open();
+                Console.WriteLine("Server On, ready to use");
+                Console.ReadLine();
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+
+
+            }
+        }
+}
